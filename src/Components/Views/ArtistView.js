43c922a93ab3f62bae8095ac1634e3c126b182e7
bhuvaneswari630 <bhuvaneswarim630@gmail.com>
2023-11-29 19:57:18 -0500

    adding fetch to album and artist view

--- src/Components/Views/ArtistView.js
+++ src/Components/Views/ArtistView.js
@@ -3,16 +3,38 @@
 // component to serve specific data about our artist
 
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 function ArtistView() {
-    const [ artistData, setArtistData ] = useState([])
+    const [artistData, setArtistData] = useState([])
     const { id } = useParams()
+    useEffect(() => {
+        const fetchData = async () => {
+            const url = `http://localhost:4000/album/${id}`
+            const response = await fetch(url)
+            const data = await response.json()
+            const albums = data.results.filter((item) => item.collectionType === 'Album')
+            // console.log(data);
+            // console.log(albums);
+            setArtistData(albums)
+        }
+        fetchData()
+    },[id])
+    const albumDisplay = artistData.map(album => {
+        return (
+            <div key={album.collectionId}>
+                {/* <p>{album.collectionName}</p> */}
+                <Link to={`/album/${album.collectionId}`}>{album.collectionName}</Link>
+
+            </div>
+        )
+    })
 
     return (
         <div>
             <p>Artist Data Goes Here!</p>
             <p>ID: {id}</p>
+            {albumDisplay}
         </div>
     )
 }
